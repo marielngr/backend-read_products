c9d076c5676cebe4da16e1920f9453166b30ef13
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-09 12:45:32 +0100

    fix env.local-Link

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,5 +1,3 @@
-import { products } from "../../../lib/products";
-
 export default function handler(request, response) {
   const { id } = request.query;
 
@@ -11,3 +9,13 @@ export default function handler(request, response) {
 
   response.status(200).json(product);
 }
+//   const { id } = request.query;
+
+//   if (request.method === "GET") {
+//     const product = await Product.findById(id);
+//   }
+
+//   if (!product) {
+//     return response.status(404).json({ status: "Not Found" });
+//   }
+//   return response.status(200).json(product);
