779dac9e096407fe86e14877f153d773c36e3c91
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-09 13:14:07 +0100

    fixed typo

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -8,8 +8,10 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const product = await Product.findById(id);
-    return response.status(200).json(product);
-  } else {
-    return response.status(404).json({ status: "Not Found" });
+
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(product);
   }
 }
